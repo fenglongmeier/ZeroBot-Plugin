09ee1acde46af67b38aa5de240bdb380a5e442e8
fenglongmeier <98135741+fenglongmeier@users.noreply.github.com>
2023-09-14 20:35:53 +0800

    Update main.go

--- main.go
+++ main.go
@@ -257,11 +257,11 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	 sus = append(sus, 1458791599)
+	   sus = append(sus, 1458791599)
 	// sus = append(sus, 87654321)
 
 	// 启用 webui
-	 go webctrl.RunGui(*g)
+	 // go webctrl.RunGui(*g)
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
